102e6c8bcc38be6e19eea7b71fc5262e15665654
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2025-05-04 03:09:39 +0300

    deployUpdate

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// var toggle = document.querySelector(".ul-list");
-//        function appear(){
-//         if(toggle.style.display=='block'){
-//           toggle.style.display='none';
-//         }
-//         else{
-//           toggle.style.display='block';
-//         }
-//        }
 
 
 function appear() {
